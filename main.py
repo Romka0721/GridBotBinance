ec1ac5adda7fba915f033a9bf092d7bf3c5dae03
Roman <romka0721@gmail.com>
2024-01-22 16:28:24 +0200

    add bot aiogram

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from API import API_KEY, API_SEKRET
 import pandas
 import asyncio
 
-from bot import send_message
+from bot import send_info
 from settings import symbol, quantity_dollars, round_tiker
 
 client = Client(API_KEY, API_SEKRET)
@@ -19,19 +19,14 @@ async def last_limit_order():
 price_test = 0
 
 
-async def price_onow():
-    global price_test
-    price_test = await last_limit_order()
-
-
 # Обчислення загального балансу в позиції
 async def balans():
     open_positions = client.futures_position_information(symbol=symbol)
     for position in open_positions:
         bal1 = float(position['positionAmt'])
-    bal1 = round(bal1 * price_test, 1)
-    print('Баланс в позиції: ', bal1)
-    return bal1
+        bal1 = round(bal1 * price_test, 1)
+        print('Баланс в позиції: ', bal1)
+        return bal1
 
 
 # Визначання збитку в позиції
@@ -44,7 +39,7 @@ async def position_on():
             quantity = float(position['positionAmt'])
             profit = round((mark_price - entry_price) * quantity, 4)
             print('Прибуток/Збиток в позиції: ', profit)
-    return profit
+            return profit
 
 
 # Визначаємо де будуть стояти лімітки
@@ -103,7 +98,6 @@ async def quantity1_short():
 async def pozity():
     poz_t1 = len(pandas.DataFrame(client.futures_get_open_orders(symbol=symbol)))
     print('Поставлено ліміток: ', poz_t1)
-    await send_message(poz_t1)
     return poz_t1
 
 
@@ -139,7 +133,10 @@ async def create_order_short():
         price=await limit_order_max(price_test),
         timeInForce='GTC'
     )
-    print(print_order)
+    price_symbol = float(print_order['origQty'])*float(print_order['price'])
+    result = (f"{print_order['symbol']}: поставив лімітку short по ціні {print_order['price']}.\n"
+              f"Кількість {str(price_symbol)}")
+    await send_info(result)
 
 
 async def create_order_long():
@@ -151,6 +148,10 @@ async def create_order_long():
         price=await limit_order_min(price_test),
         timeInForce='GTC'
     )
+    price_symbol = float(print_order['origQty']) * float(print_order['price'])
+    result = (f"{print_order['symbol']}: поставив лімітку long по ціні {print_order['price']}.\n"
+              f"Кількість {str(price_symbol)}")
+    await send_info(result)
     print(print_order)
 
 
@@ -159,25 +160,23 @@ async def main():
         try:
             global price_test
             if await position_on() > quantity_dollars * -2:
+                await asyncio.sleep(30)
                 if await pozity() < 2:
                     await asyncio.sleep(120)
                     client.futures_cancel_all_open_orders(symbol=symbol)
                     price_test = await last_limit_order()
+                    result = f'Останній закритий ордер по ціні {str(price_test)}'
+                    await send_info(result)
                     await create_order_long()
                     await create_order_short()
-                    result_long = f'Поставив ліиітку long: {limit_order_max(price_test)}'
-                    result_short = f'Поставив ліиітку long: {limit_order_min(price_test)}'
-                    await send_message(result_long)
-                    await send_message(result_short)
                 else:
                     print('Чекаю')
-                    await asyncio.sleep(120)
             else:
                 await clouse_pozision()
                 break
         except:
             print('Помилка!')
-            await asyncio.sleep(1)
+            await asyncio.sleep(10)
 
 
 if __name__ == "__main__":

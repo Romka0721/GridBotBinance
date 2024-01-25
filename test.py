a2a3e851d10b38143d6a31d55743fb7867063944
Roman <romka0721@gmail.com>
2024-01-25 12:34:56 +0200

    dshzdjkolkgbj

--- test.py
+++ test.py
@@ -4,10 +4,6 @@ from API import API_KEY, API_SEKRET
 client = Client(API_KEY, API_SEKRET)
 
 
-SYMBOL = 'SUIUSDT'
-QU_DOLLARS = 30
-
-
 class SymbolInfo:
     def __init__(self, symbol):
         self.symbol = symbol
@@ -30,11 +26,49 @@ class LimitOrderPosition:
     def __init__(self, symbol, dollars):
         self.symbol = symbol
         self.dollars = dollars
+        self.last_limit_order = SymbolInfo(self.symbol).last_open_limit_order()
+        self.balance_on_position = SymbolInfo(self.symbol).balance_on_position()[0]
+        self.quantity_coin = round(self.dollars / self.last_limit_order, 2)
+        if self.symbol == '1000PEPEUSDT':
+            self.round_tiker = 7
+        else:
+            self.round_tiker = 4
 
     def limit_order_long(self):
-        balance_on_position = SymbolInfo.balance_on_position(self.symbol)
+        if self.balance_on_position < self.dollars * -5 or self.balance_on_position > self.dollars * 3:
+            limit_order_long = round(self.last_limit_order - (self.last_limit_order * 0.06), self.round_tiker)
+        elif self.balance_on_position < self.dollars * -3 or self.balance_on_position > self.dollars * 2:
+            limit_order_long = round(self.last_limit_order - (self.last_limit_order * 0.045), self.round_tiker)
+        else:
+            limit_order_long = round(self.last_limit_order - (self.last_limit_order * 0.03), self.round_tiker)
+        return limit_order_long
 
-        print(balance_on_position)
+    def limit_order_short(self):
+        if self.balance_on_position < self.dollars * -3 or self.balance_on_position > self.dollars * 5:
+            limit_order_short = round(self.last_limit_order + (self.last_limit_order * 0.06), self.round_tiker)
+        elif self.balance_on_position < self.dollars * -2 or self.balance_on_position > self.dollars * 3:
+            limit_order_short = round(self.last_limit_order + (self.last_limit_order * 0.045), self.round_tiker)
+        else:
+            limit_order_short = round(self.last_limit_order + (self.last_limit_order * 0.03), self.round_tiker)
+        return limit_order_short
+
+    def quantity_order_long(self):
+        if self.balance_on_position > self.dollars * 3:
+            quantity_order_long = round(10 / self.last_limit_order)
+        elif self.balance_on_position > self.dollars * 1.6:
+            quantity_order_long = round((self.dollars * 0.6) / self.last_limit_order)
+        else:
+            quantity_order_long = round(self.quantity_coin)
+        return quantity_order_long
+
+    def quantity_order_short(self):
+        if self.balance_on_position < self.dollars * -3:
+            quantity_order_short = round(10 / self.last_limit_order)
+        elif self.balance_on_position < self.dollars * -1.6:
+            quantity_order_short = round((self.dollars * 0.6) / self.last_limit_order)
+        else:
+            quantity_order_short = round(self.quantity_coin)
+        return quantity_order_short
 
 
 class CreateOrder:
@@ -65,6 +99,9 @@ class CreateOrder:
         )
         return create_order
 
+    def delete_orders(self):
+        client.futures_cancel_all_open_orders(symbol=self.symbol)
+
     def create_order_close_position(self):
         client.futures_cancel_all_open_orders(symbol=self.symbol)
         open_positions = client.futures_position_information(symbol=self.symbol)
@@ -81,13 +118,3 @@ class CreateOrder:
                 quantity=quantity
             )
             return create_order
-
-
-def main():
-    while True:
-        pass
-
-
-if __name__ == "__main__":
-    trade_work = SymbolInfo(SYMBOL)
-    trade_work_position = LimitOrderPosition(SYMBOL, QU_DOLLARS)
---
+++ test2.py
@@ -0,0 +1,30 @@
+from time import sleep
+from test import SymbolInfo, LimitOrderPosition, CreateOrder
+
+SYMBOL = 'SUIUSDT'
+QU_DOLLARS = 30
+
+
+def main():
+    while True:
+        if trade_work.balance_on_position()[1] > QU_DOLLARS*-2:
+            sleep(2)
+            price_test = trade_work.last_open_limit_order()
+            print(price_test, '1')
+            if trade_work.open_quantity_position() < 2:
+                sleep(2)
+                create_order.delete_orders()
+                price_test = trade_work.last_open_limit_order()
+                print(price_test, '2')
+            else:
+                pass
+
+        else:
+            pass
+
+
+if __name__ == "__main__":
+    trade_work = SymbolInfo(SYMBOL)
+    trade_work_position = LimitOrderPosition(SYMBOL, QU_DOLLARS)
+    create_order = CreateOrder(SYMBOL, trade_work_position.quantity_order_long(), trade_work_position.limit_order_long())
+    main()
